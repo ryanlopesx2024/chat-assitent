62663ddf6cc0174cd0a691472b7fa11b3e3ed942
ryanlopesx2024 <ryanlopesnegocios@gmail.com>
2025-02-05 20:55:39 -0300

    Fix: Update build configuration to resolve white screen issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', '@mui/material', '@emotion/react', '@emotion/styled'],
           utils: ['jspdf', 'html2canvas']
-        }
+        },
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
       }
     }
   },
